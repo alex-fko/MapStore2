fc0697afa8a345a7032d49c67caa3519d0df44d0
Alexander <mineralex1991@gmail.com>
2022-06-15 16:28:06 +0300

    #7928 Corrections to the swipable-view content for mobile view
    - Layer selector of identify tool will list layers ONLY with valid responses. Invalid ones won't be hidden with CSS but will not be listed as an option at all.
    - Identify panel content will not render empty panel per each requested layer. It will also render data ONLY for valid responses.
    - This changes index numbers, but it's ok because of no more hidden empty panels for invalid responses.

--- web/client/reducers/mapInfo.js
+++ web/client/reducers/mapInfo.js
@@ -42,7 +42,7 @@ import {
 
 import { MAP_CONFIG_LOADED } from '../actions/config';
 import { RESET_CONTROLS } from '../actions/controls';
-import { MAP_TYPE_CHANGED } from './../actions/maptype';
+import { MAP_TYPE_CHANGED } from '../actions/maptype';
 
 import { getValidator } from '../utils/MapInfoUtils';
 
@@ -113,7 +113,7 @@ function receiveResponse(state, action, type) {
         if (isHover) {
             indexObj = {loaded: true, index: 0};
         } else if (!isHover && isIndexValid(state, responses, requestIndex, isVector)) {
-            indexObj = {loaded: true, index: requestIndex};
+            indexObj = {loaded: true, index: 0};
         } else if (responses.length === requests.length && !indexObj?.loaded) {
             // if all responses are empty hence valid but with no valid index
             // then set loaded to true
